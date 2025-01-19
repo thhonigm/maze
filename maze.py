13a9e97e9f7acecd2e2cce0457d90d0230bc4ea9
Thomas Honigmann <thonigmann@gmail.com>
2025-01-19 19:57:54 +0100

    L11; Reset the Cell's "visited" Property

--- maze.py
+++ maze.py
@@ -2,7 +2,12 @@ import time
 from tkinter import Tk, Canvas
 import random
 
+CELL_COLOR = "black"
 BG_COLOR = "#d9d9d9"
+MOVE_COLOR = "red"
+SHOW_NOT_VISITED = False
+DRAW_INVISIBLE = True
+
 
 class Window:
 
@@ -18,6 +23,10 @@ class Window:
         self.__root.update_idletasks()
         self.__root.update()
 
+    def animate(self):
+        self.redraw()
+        time.sleep(0.05)
+
     def wait_for_close(self):
         self.is_running = True
         while self.is_running:
@@ -26,14 +35,16 @@ class Window:
     def close(self):
         self.is_running = False
 
-    def draw_line(self, line, fill_color = "black"):
+    def draw_line(self, line, fill_color):
         line.draw(self.canvas, fill_color)
+
 
 class Point:
 
     def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
+
 
 class Line:
 
@@ -48,6 +59,7 @@ class Line:
                            fill = fill_color,
                            width = 2)
 
+
 class Cell:
 
     LEFT = 0
@@ -58,59 +70,70 @@ class Cell:
 
     SIZE = 40
 
-    SHOW_NOT_VISITED = False
-
-    def __init__(self, p, win = None):
+    def __init__(self, p):
         self.p = p
-        self.__win = win
         self.has_wall = [True, True, True, True]
         self.visited = False
 
-    def _draw_line(self, p1, p2, visible):
-        if visible:
-            self.__win.draw_line(Line(p1, p2))
-        else:
-            self.__win.draw_line(Line(p1, p2), BG_COLOR)
-
-    def draw(self):
-        self._draw_line(
-            self.p,
+    def draw(self, win, animate = False):
+        if win is None:
+            return
+        lines = {True: [], False: []}
+        lines[self.has_wall[self.LEFT]].append((
+            Point(self.p.x, self.p.y),
             Point(self.p.x, self.p.y + self.SIZE),
-            self.has_wall[self.LEFT]
-        )
-        self._draw_line(
-            self.p,
+        ))
+        lines[self.has_wall[self.TOP]].append((
+            Point(self.p.x, self.p.y),
             Point(self.p.x + self.SIZE, self.p.y),
-            self.has_wall[self.TOP]
-        )
-        self._draw_line(
+        ))
+        lines[self.has_wall[self.RIGHT]].append((
             Point(self.p.x + self.SIZE, self.p.y),
             Point(self.p.x + self.SIZE, self.p.y + self.SIZE),
-            self.has_wall[self.RIGHT]
-        )
-        self._draw_line(
+        ))
+        lines[self.has_wall[self.BOTTOM]].append((
             Point(self.p.x, self.p.y + self.SIZE),
             Point(self.p.x + self.SIZE, self.p.y + self.SIZE),
-            self.has_wall[self.BOTTOM]
-        )
-        if Cell.SHOW_NOT_VISITED:
-            self._draw_line(
-                Point(self.p.x + self.SIZE//4, self.p.y + self.SIZE//4),
-                Point(self.p.x + 3*self.SIZE//4, self.p.y + 3*self.SIZE//4),
-                not self.visited
-            )
-            self._draw_line(
-                Point(self.p.x + 3*self.SIZE//4, self.p.y + self.SIZE//4),
-                Point(self.p.x + self.SIZE//4, self.p.y + 3*self.SIZE//4),
-                not self.visited
-            )
+        ))
+        if DRAW_INVISIBLE:
+            for p, q in lines[False]:
+                if p.x == q.x:
+                    p.y += 1
+                    q.y -= 1
+                else:
+                    p.x += 1
+                    q.x -= 1
+                win.draw_line(Line(p, q), BG_COLOR)
+        for p, q in lines[True]:
+            win.draw_line(Line(p, q), CELL_COLOR)
+        if SHOW_NOT_VISITED:
+            for line in ((
+                Line(Point(self.p.x + self.SIZE//4, self.p.y + self.SIZE//4),
+                     Point(self.p.x + 3*self.SIZE//4, self.p.y + 3*self.SIZE//4)),
+                Line(Point(self.p.x + 3*self.SIZE//4, self.p.y + self.SIZE//4),
+                    Point(self.p.x + self.SIZE//4, self.p.y + 3*self.SIZE//4))
+            )):
+                if self.visited:
+                    if DRAW_INVISIBLE:
+                        win.draw_line(line, BG_COLOR)
+                else:
+                    win.draw_line(line, CELL_COLOR)
+        if animate:
+            win.animate()
 
-    def draw_move(self, to_cell, undo = False):
+    def draw_move(self, to_cell, win, undo = False, animate = False):
+        if win is None:
+            return
         center = Point(self.p.x + self.SIZE//2, self.p.y + self.SIZE//2)
         to_center = Point(to_cell.p.x + self.SIZE//2, to_cell.p.y + self.SIZE//2)
-        self.__win.draw_line(Line(center, to_center), "grey" if undo else "red")
+        win.draw_line(Line(center, to_center), BG_COLOR if undo else MOVE_COLOR)
+        if animate:
+            win.animate()
+
 
 class Maze:
+
+    ANIMATE_INIT = False
 
     def __init__(
         self,
@@ -133,31 +156,24 @@ class Maze:
         self.cell_size_y = cell_size_y
         self.__win = win
         self._create_cells()
+        self._break_entrance_and_exit()
+        self._break_walls_r()
+        self._reset_cells_visited()
 
     def _create_cells(self):
         self._cells = []
         for c in range(self.num_cols):
             self._cells.append([])
             for r in range(self.num_rows):
-                p = Point(self.x1 + c * self.cell_size_x,
-                          self.y1 + r * self.cell_size_y)
-                self._cells[-1].append(Cell(p, self.__win))
-        for c in range(self.num_cols):
-            for r in range(self.num_rows):
-                self._draw_cell(c, r)
-
-    def _draw_cell(self, c, r):
-        if self.__win is None:
-            return
-        self._cells[c][r].draw()
+                cell = Cell(
+                    Point(self.x1 + c * self.cell_size_x,
+                          self.y1 + r * self.cell_size_y))
+                self._cells[-1].append(cell)
+                cell.draw(self.__win, self.ANIMATE_INIT)
 
     def _wall_down(self, c, r, d):
         self._cells[c][r].has_wall[d] = False
-        self._draw_cell(c, r)
-
-    def _animate(self):
-        self.__win.redraw()
-        time.sleep(0.05)
+        self._cells[c][r].draw(self.__win, self.ANIMATE_INIT)
 
     def _break_entrance_and_exit(self):
         self._wall_down(0, 0, Cell.TOP)
@@ -183,8 +199,6 @@ class Maze:
         return False, 0, 0, 0
 
     def _break_walls_r(self, i = 0, j = 0):
-        if self._cells[i][j].visited:
-            raise Exception(f"({i}, {j}) already visited")
         self._cells[i][j].visited = True
         while True:
             cells_to_visit = []
@@ -193,50 +207,63 @@ class Maze:
                 if to_visit:
                     cells_to_visit.append((d, ii, jj, dd))
             if len(cells_to_visit) == 0:
-                self._draw_cell(i, j)
+                self._cells[i][j].draw(self.__win, self.ANIMATE_INIT)
                 return
             d, ii, jj, dd = random.choice(cells_to_visit)
             self._wall_down(i, j, d)
             self._wall_down(ii, jj, dd)
+            self._cells[i][j].draw_move(self._cells[ii][jj], self.__win,
+                                        animate = self.ANIMATE_INIT)
             self._break_walls_r(ii, jj)
+            self._cells[i][j].draw_move(self._cells[ii][jj], self.__win, undo = True,
+                                        animate = self.ANIMATE_INIT)
+
+    def _reset_cells_visited(self):
+        for c in self._cells:
+            for cr in c:
+                cr.visited = False
+                if self.ANIMATE_INIT:
+                    cr.draw(self.__win)
 
 
 def main1():
     win = Window(800, 600)
+    win.animate()
 
     for i in range(17):
         for j in range(11):
-            c = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE), win)
+            c = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE))
             for side in range(4):
                 if (i+j) & (1 << side):
                     c.has_wall[side] = False
-            c.draw()
+            c.draw(win)
 
     i, j = 2, 3
-    c1 = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE), win)
+    c1 = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE))
     i, j = 2, 8
-    c2 = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE), win)
-    c1.draw_move(c2)
+    c2 = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE))
+    c1.draw_move(c2, win, animate = True)
 
     i, j = 12, 8
-    c3 = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE), win)
-    c2.draw_move(c3)
+    c3 = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE))
+    c2.draw_move(c3, win, animate = True)
 
     i, j = 12, 3
-    c4 = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE), win)
-    c3.draw_move(c4)
+    c4 = Cell(Point(10 + i*Cell.SIZE, 30 + j*Cell.SIZE))
+    c3.draw_move(c4, win, animate = True)
+    c4.draw_move(c1, win, animate = True)
+    time.sleep(2.5)
 
-    c4.draw_move(c1, undo=True)
+    c4.draw_move(c1, win, undo=True, animate = True)
+    c3.draw_move(c4, win, undo=True, animate = True)
+    c2.draw_move(c3, win, undo=True, animate = True)
+    c1.draw_move(c2, win, undo=True, animate = True)
 
     win.wait_for_close()
 
 def main():
     win = Window(800, 600)
-
-    maze = Maze(20, 25, 6, 10, Cell.SIZE, Cell.SIZE, win)
-    maze._break_entrance_and_exit()
-    maze._break_walls_r()
-
+    maze = Maze(20, 25, 10, 13, Cell.SIZE, Cell.SIZE, win)
     win.wait_for_close()
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -21,44 +21,22 @@ class Tests(unittest.TestCase):
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         m1._break_entrance_and_exit()
-        self.assertEqual(
-            m1._cells[0][0].has_wall[Cell.TOP],
-            False,
-        )
-        self.assertEqual(
-            m1._cells[0][0].has_wall[Cell.LEFT],
-            True,
-        )
-        self.assertEqual(
-            m1._cells[0][0].has_wall[Cell.BOTTOM],
-            True,
-        )
-        self.assertEqual(
-            m1._cells[0][0].has_wall[Cell.RIGHT],
-            True,
-        )
+        self.assertFalse(m1._cells[0][0].has_wall[Cell.TOP])
 
     def test_maze_exit(self):
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        m1._break_entrance_and_exit()
-        self.assertEqual(
-            m1._cells[num_cols - 1][num_rows - 1].has_wall[Cell.BOTTOM],
-            False,
-        )
-        self.assertEqual(
-            m1._cells[num_cols - 1][num_rows - 1].has_wall[Cell.TOP],
-            True,
-        )
-        self.assertEqual(
-            m1._cells[num_cols - 1][num_rows - 1].has_wall[Cell.LEFT],
-            True,
-        )
-        self.assertEqual(
-            m1._cells[num_cols - 1][num_rows - 1].has_wall[Cell.RIGHT],
-            True,
-        )
+        self.assertFalse(m1._cells[num_cols - 1][num_rows - 1].has_wall[Cell.BOTTOM])
+
+    def test_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in range(num_cols):
+            for row in range(num_rows):
+                self.assertFalse(m1._cells[col][row].visited)
+
 
 if __name__ == "__main__":
     unittest.main()
